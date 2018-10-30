8e8cb3bb0b01f7d931593c110fac7ec04def3685
borismattijssen <borismattijssen@gmail.com>
2018-10-30 14:08:50 +0100

    Adds start of rmd for 1.3b

--- homework-1.3/exercise-1.3b.R
+++ homework-1.3/exercise-1.3b.R
@@ -12,4 +12,11 @@ library(factoextra)
 cars.pca=PCA(cars.pc,quali.sup=8,ncp=5,scale.unit=TRUE, graph=FALSE)
 
 # five principal component scores:
-cars.pca$ind$coord
+# cars.pca$ind$coord
+
+# most variance is explained by the first PC, while the 
+cars.pca$eig
+
+fviz_pca_ind(cars.pca, label="none", col.ind = "contrib")
+fviz_pca_ind(cars.pca, label="none", habillage="origin",
+             addEllipses=TRUE, ellipse.level=0.95)
